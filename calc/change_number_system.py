8df8413c91e0b7144312757f8da54f91bb44f6f8
Ilya <ilyaanashkin17@gmail.com>
2024-02-13 09:11:42 +0300

    added functions for changing the number system

---
+++ calc/change_number_system.py
@@ -0,0 +1,42 @@
+import math
+
+alph = '0123456789ABCDEF'
+
+def to_dec(num: str, radix: int) -> int:
+
+    tmp = 1
+    res = 0
+
+    for digit in num[::-1]:
+        res += alph.index(digit) * tmp
+        tmp *= radix
+
+    return res
+
+
+def dec_to_other(num: float, radix: int) -> str:
+    str_num = str(num)
+    fractional_part, whole_part = math.modf(num)
+    res = 0
+    ans = ''
+
+    whole_part = int(whole_part)
+
+    while whole_part >= radix:
+        ans += alph[whole_part % radix]
+        whole_part //= radix
+
+    ans += alph[whole_part]
+
+    ans = ans[::-1]
+
+    ans += '.'
+
+    for i in range(10):
+        fractional_part *= radix
+        tmp = math.modf(fractional_part)
+        ans += alph[int(tmp[1])]
+        fractional_part = tmp[0]
+
+    return ans
+
