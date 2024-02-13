e8fe3bf0b2c52f669bbbaabda84b1bba909355a2
Чекунова Марьяна <113853811+marjana2213@users.noreply.github.com>
2024-02-13 20:45:17 +0300

    Update egyptian_number_system.py

--- calc/egyptian_number_system.py
+++ calc/egyptian_number_system.py
@@ -14,20 +14,26 @@ def egyptian_converter_to_dec(word: str):
 #перевод из десятичной в египт. СС
 def dec_converter_to_egyptian(num: int):
     result = ''
-    temp_num = num
-    value_index = 1
+    str_number = str(num)
+    if '.' in str_number:
+        integer_part, decimal_part = str_number.split('.')
+        if decimal_part == '0':
+            num2 = int(integer_part)
+            temp_num = num2
+            value_index = 1
+            while temp_num > 0:
+                temp = temp_num % 10
+                temp_num //= 10
+                for i in range(temp):
+                    result += list(words_values.keys())[list(words_values.values()).index(value_index)]
+                value_index *= 10
 
-    while temp_num > 0:
-        temp = temp_num % 10
-        temp_num //= 10
-        for i in range(temp):
-            result += list(words_values.keys())[list(words_values.values()).index(value_index)]
-        value_index *= 10
-
-    if result == '':
-        return '0'
-    else:
-        return result[::-1]
+            if result == '':
+                return '0'
+            else:
+                return result[::-1]
+        else:
+            return 'Ошибка'
 
 #проверка для всех СС
 alph = '0123456789ABCDEFI^P?!JH'
