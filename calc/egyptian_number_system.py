dc1bb09579c0539b5a09e93742dad1a8a49ab097
daryavanina <114079693+daryavanina@users.noreply.github.com>
2024-02-14 11:30:30 +0300

    Update egyptian_number_system.py

--- calc/egyptian_number_system.py
+++ calc/egyptian_number_system.py
@@ -17,7 +17,7 @@ def egyptian_converter_to_dec(word: str):
 def dec_converter_to_egyptian(num: int):
     result = ''
     str_number = str(num)
-    if '.' in str_number:
+    if '.' in str_number: #проверка на дробное число
         integer_part, decimal_part = str_number.split('.')
         if decimal_part == '0':
             num2 = int(integer_part)
@@ -36,6 +36,23 @@ def dec_converter_to_egyptian(num: int):
                 return result[::-1]
         else:
             return 'Ошибка'
+    else: #перевод целого числа
+        result = ''
+        temp_num = num
+        value_index = 1
+
+        while temp_num > 0:
+            temp = temp_num % 10
+            temp_num //= 10
+            for i in range(temp):
+                result += list(words_values.keys())[list(words_values.values()).index(value_index)]
+            value_index *= 10
+
+        if result == '':
+            return '0'
+        else:
+            return result[::-1]
+
 
 #Проверка для всех СС(с 2 по 16, египетс.) на то, что введено число под нужную(выбранную) СС
 alph = '0123456789ABCDEFI^P?!JH' #алфавит наших СС
