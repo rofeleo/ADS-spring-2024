aa94473d080cfc8818780a1fbad966e3409d38e0
Ilya <ilyaanashkin17@gmail.com>
2024-02-14 11:46:14 +0300

    added comments

--- calc/change_number_system.py
+++ calc/change_number_system.py
@@ -2,6 +2,7 @@ import math
 
 alph = '0123456789ABCDEF'
 
+# функция переводит число в десятичную СС
 def to_dec(num: str, radix: int) -> int:
 
     tmp = 1
@@ -14,15 +15,17 @@ def to_dec(num: str, radix: int) -> int:
     return res
 
 
+# функция переводит число из десятичной СС (число м. б. дробным) в любую
 def dec_to_other(num: float, radix: int) -> str:
     num = float(num)
     str_num = str(num)
-    fractional_part, whole_part = math.modf(num)
+    fractional_part, whole_part = math.modf(num) # выделяем целую и дробную части числа
     res = 0
     ans = ''
 
     whole_part = int(whole_part)
 
+    # переводим целую часть числа
     while whole_part >= radix:
         ans += alph[whole_part % radix]
         whole_part //= radix
@@ -31,11 +34,13 @@ def dec_to_other(num: float, radix: int) -> str:
 
     ans = ans[::-1]
 
+    # если дробная часть отсутствует, возвращаем овтет
     if num.is_integer():
         return ans
 
     ans += '.'
 
+    # переводим дробную часть (точность 9 знаков [10-й не округляется])
     for i in range(10):
         fractional_part *= radix
         tmp = math.modf(fractional_part)
