81aa878b0977c033cd00e4c8b9e3676565a02df3
Чекунова Марьяна <113853811+marjana2213@users.noreply.github.com>
2024-02-27 00:11:55 +0300

    Update comb_chekunova.py

--- schemes/comb_chekunova.py
+++ schemes/comb_chekunova.py
@@ -19,11 +19,6 @@ def placements_without_repetitions(n, k):
     ans.add(perm)
   return(ans)
 
-def combinations(n, k):
-  ans = set()
-  for i in itertools.combinations(range(1, n + 1), k):
-    ans.add(i)
-  return ans
 
 
 
