fdc5fe1d3a9e9d4cf3bd129055af99f2b61ef548
daryavanina <114079693+daryavanina@users.noreply.github.com>
2024-02-27 02:04:43 +0300

    Update comb_vanina.py

--- schemes/comb_vanina.py
+++ schemes/comb_vanina.py
@@ -4,13 +4,8 @@ def sum_rule (set1, set2):
     num2 = set(set2.split(","))
     return sorted(num1|num2)
 
-#result_set = sum_rule("7,3","5,5")
-#print(*result_set, sep=", ")
-
 #правило произведения
 def multi_rule (set1, set2):
     num1 = (set1.split(","))
     num2 = (set2.split(","))
     return len(num1)*len(num2)
-
-#print(multi_rule("1,1","1,2"))
